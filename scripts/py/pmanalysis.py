4c729d59f685a94584dd3bfc5def5d085964ae10
dacuevas <dcuevas08@gmail.com>
2013-12-28 12:39:33 -0800

    pmanalysis.py: - Using argparse instead of getopt.

--- scripts/py/pmanalysis.py
+++ scripts/py/pmanalysis.py
@@ -1,58 +1,63 @@
 #!/usr/bin/python
 # pmanalysis.py
+# Main driver for the PM analysis
+#
 # Author: Daniel A Cuevas
 # Created on 22 Nov. 2013
+# Updated on 28 Dec. 2013
 
+import argparse
+import PMData
 import sys
-import os
-import getptopt
+import time
+import datetime
 
 
-def usage():
-    '''Usage message for failed script execution'''
-	print "\nusage: %s -i inputfile -o outputname -n output_directory\n\n" \
-            % os.path.basename(sys.argv[0])
+###############################################################################
+# Utility methods
+###############################################################################
 
+def timeStamp():
+    '''Return ime stamp'''
+    t = time.time()
+    fmt = '[%Y-%m-%d %H:%M:%S]'
+    return datetime.datetime.fromtimestamp(t).strftime(fmt)
+
+
+def printStatus(msg):
+    '''Print status message'''
+    print >> sys.stderr, timeStamp(), ' ', msg
 
 ###############################################################################
 # Argument Parsing
 ###############################################################################
 
-inputFile = None
-outPrefix = None # Prefix for all output files
-outputDir = None
-filterFlag = False
-newHMFlag = False
-window_size = 3
-try:
-	opts, args = getopt.getopt(sys.argv[1:],'fhi:o:n:z')
-except getopt.GetoptError:
-	usage()
-	sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        usage()
-        sys.exit(2)
-    elif opt == '-i':
-        inputFile = arg
-    elif opt == '-o':
-        outPrefix = arg
-    elif opt == '-n':
-        outputDir = arg
-    elif opt == '-f':
-        filterFlag = True
-    elif opt == '-z':
-        newHMFlag = True
-    else:
-        pass
+parser = argparse.ArgumentParser()
+parser.add_argument('infile', help='Input PM file')
+parser.add_argument('outdir',
+                    help='Directory to store output files')
+parser.add_argument('-o', '--outprefix',
+                    help='Prefix prepended to output files')
+parser.add_argument('-f', '--filter', action='store_true',
+                    help='Apply filtering to growth curves')
+parser.add_argument('-g', '--newgrowth', action='store_true',
+                    help='Apply new growth level calculation')
 
-# Check if arguments were not given
-if not inputFile or not outputPrefix or not outputDir:
-	usage()
-	sys.exit(2)
+args = parser.parse_args()
+inputFile = args.infile
+outPrefix = args.outprefix if args.outprefix else 'out'
+outDir = args.outdir
+filterFlag = args.filter
+newGrowthFlag = args.newgrowth
+verbFlag = args.verbose
+window_size = 3
 
 ###############################################################################
 # Data Processing
 ###############################################################################
 
 # Parse data file
+printStatus('Parsing input file.')
+sys.stderr.flush()
+pmdata = PMData.PMData(inputFile)
+printStatus('Parsing complete.')
